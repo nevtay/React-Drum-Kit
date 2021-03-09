cd02a6db145b09e870c93d5cb5195a367586745f
Nevin Tay <nevintay@gmail.com>
2021-03-10 00:01:31 +0800

    Apply formatting rules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import "./App.css";
+import React from "react"
+import "./App.css"
 
 function App() {
   return (
     <div>
       <h1>React Drum Kit</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
