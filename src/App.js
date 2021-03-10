1b7c7630b09af5899d87b3ed09be58bfc7840d0d
Nevin Tay <nevintay@gmail.com>
2021-03-11 00:59:54 +0800

    Add test function for exploring how to play sound file from a mock server

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React from "react"
 import "./App.css"
 
 function App() {
+  const fetchClap = async () => {
+    const res = await fetch("./instruments")
+    const data = await res.json()
+    console.log(data[0])
+    const audio2 = await new Audio(data[0].file)
+    await audio2.play()
+  }
   return (
     <div>
       <h1>React Drum Kit</h1>
+      <button
+        onClick={() => {
+          fetchClap()
+        }}
+      >
+        Clap
+      </button>
     </div>
   )
 }
