2715aaf08bef2598954efa9adb369e8f2ef38e8a
Nevin Tay <nevintay@gmail.com>
2021-03-12 15:12:08 +0800

    Initial commit

---
+++ src/hooks/useKeypress.js
@@ -0,0 +1,7 @@
+/**
+ * useKeyPress
+ * @param {string} key - the name of the key to respond to; to be compared against event.key
+ * @param {function} action - the action to perform on keypress
+ */
+
+export default function useKeypress(key, action) {}
