60194d4f2300825e2db102b5935750621441cd4d
Nevin Tay <nevintay@gmail.com>
2021-03-12 15:18:09 +0800

    Add custom hook logic

--- src/hooks/useKeypress.js
+++ src/hooks/useKeypress.js
@@ -1,7 +1,22 @@
+import { useEffect } from "react"
+
 /**
  * useKeyPress
  * @param {string} key - the name of the key to respond to; to be compared against event.key
  * @param {function} action - the action to perform on keypress
  */
 
-export default function useKeypress(key, action) {}
+export default function useKeypress(key, action) {
+  useEffect(() => {
+    // * define event listener
+    const onKeyup = (e) => {
+      if (e.key === key) action()
+    }
+
+    // * register event listener
+    window.addEventListener("keyup", onKeyup)
+
+    // * unregister event listener
+    return () => window.removeEventListener("keyup", onKeyup)
+  }, [])
+}
