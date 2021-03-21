658a7a5b8bbc6e4655385d0d56617de95cde3573
Nevin Tay <nevintay@gmail.com>
2021-03-21 21:19:47 +0800

    Create layout for rendering individual instrument

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,7 +1,10 @@
+/* eslint react/prop-types: 0 */
 import React from "react"
 
-const Key = () => {
-  return <div></div>
-}
+const Key = ({ instrument }) => (
+  <div>
+    <p>{instrument.name}</p> <p>{instrument.keyboardPosition}</p>
+  </div>
+)
 
 export default Key
