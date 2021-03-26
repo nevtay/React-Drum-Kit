9b34a61d9018bd4623162ba9b652d1ae597ba448
Nevin Tay <nevintay@gmail.com>
2021-03-26 17:08:14 +0800

    Initial commit

---
+++ src/components/KeyHistory.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react"
+
+const KeyHistory = ({ keys = [] }) => {
+  const [currentKeys, setCurrentKeys] = useState([])
+
+  useEffect(() => {
+    const lastTenKeys = keys.filter((k, i) => i < 10)
+    setCurrentKeys([...lastTenKeys])
+  }, [currentKeys])
+
+  return (
+    <>
+      <h2>Last Ten Keys</h2>
+      {currentKeys.length > 0 ? <p>{currentKeys.join(" > ")}</p> : <p>Play something!</p>}
+    </>
+  )
+}
+
+export default KeyHistory
