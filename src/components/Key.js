bd45634c0964efa6a25d12ee9158b350e919c5cb
Nevin Tay <nevintay@gmail.com>
2021-03-28 23:36:32 +0800

    Add default value of an empty object to 'instrument' prop

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,7 +1,7 @@
 /* eslint react/prop-types: 0 */
 import React from "react"
 
-const Key = ({ instrument }) => (
+const Key = ({ instrument = {} }) => (
   <div className="instrument-single">
     <span className="instrument-name">{instrument.name}</span>
     <span className="instrument-key">{instrument.keyboardPosition}</span>
