9c3446e5672498d254bb13de18e3c9c1bb889ff2
Nevin Tay <nevintay@gmail.com>
2021-03-28 23:01:15 +0800

    Refactor code that shows last 10 played keys

--- src/components/KeyHistory.js
+++ src/components/KeyHistory.js
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from "react"
 
 const KeyHistory = ({ keys = [] }) => {
-  const [currentKeys, setCurrentKeys] = useState([])
-
-  useEffect(() => {
-    const lastTenKeys = keys.filter((k, i) => i < 10)
-    setCurrentKeys([...lastTenKeys])
-  }, [])
-
+  const showLast10Keys = [...keys]
+    .filter((key, i) => i < 10)
+    .reverse()
+    .join(" > ")
   return (
     <>
       <h2>Last Ten Keys</h2>
-      {currentKeys.length > 0 ? <p>{currentKeys.join(" > ")}</p> : <p>Play something!</p>}
+      {keys.length > 0 ? <p>{showLast10Keys}</p> : <p>Play something!</p>}
     </>
   )
 }
