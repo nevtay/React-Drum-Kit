c6ce1f785654f9a410f8c8391759fa8fae7ffb92
Nevin Tay <nevintay@gmail.com>
2021-04-03 22:13:26 +0800

    Add attribution to icon provider

--- src/App.js
+++ src/App.js
@@ -34,6 +34,16 @@ function App() {
         <Keys instruments={instruments} setKeys={setKeys} />
       </div>
       <KeyHistory keys={keys} />
+      <footer>
+        Icons made by{" "}
+        <a href="https://www.flaticon.com/authors/icongeek26" title="Icongeek26">
+          Icongeek26
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </footer>
     </div>
   )
 }
