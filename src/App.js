8264308b44c8198e42fad770266b5afc940ebd6b
Nevin Tay <nevintay@gmail.com>
2021-04-04 22:43:12 +0800

    Abstract resize event listener into a separate useEffect() for better readability

--- src/App.js
+++ src/App.js
@@ -20,17 +20,24 @@ function App() {
    * @description fetches instrument metadata
    */
   useEffect(() => {
-    let vh = window.innerHeight * 0.01
-    document.documentElement.style.setProperty("--vh", `${vh}px`)
-    window.addEventListener("resize", () => {
-      let vh = window.innerHeight * 0.01
-      document.documentElement.style.setProperty("--vh", `${vh}px`)
-    })
     try {
       fetchinstruments()
     } catch (err) {
       throw new Error(err)
     }
+  }, [])
+
+  /**
+   * @description updates a css variable based on the innerHeight of the global window object
+   */
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty("--vh", `${vh}px`)
+    window.addEventListener("resize", () => {
+      vh = window.innerHeight * 0.01
+      console.log(`vh is ${window.innerHeight}px`)
+      document.documentElement.style.setProperty("--vh", `${vh}px`)
+    })
   }, [])
 
   return (
