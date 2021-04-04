36ec5adebd69596300296be3d84f801e4c7a95ba
Nevin Tay <nevintay@gmail.com>
2021-04-04 18:12:26 +0800

    Remove abstracted setKey() function as <Key> component instances handle updating of pressed keys

--- src/components/Keys.js
+++ src/components/Keys.js
@@ -1,34 +1,11 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import Key from "./Key"
 
 const Keys = ({ instruments, setKeys }) => {
-  const validKeys = [...instruments].map((instrument) => instrument.keyboardPosition)
-
-  const updateKeysHistory = (e) => {
-    instruments.map((item) => {
-      if (item.keyboardPosition === e.key) {
-        setKeys((prev) => [item.keyboardPosition, ...prev])
-      }
-      return null
-    })
-  }
-
-  /**
-   * @description sets "keydown" event listener if instruments have been loaded from server
-   */
-  useEffect(() => {
-    if (!instruments.length) {
-      return null
-    }
-    window.addEventListener("keydown", (e) => {
-      updateKeysHistory(e)
-    })
-  }, [instruments])
-
   return instruments.map((instrument) => {
     return (
       <div key={instrument.id}>
-        <Key instrument={instrument} />
+        <Key instrument={instrument} setKeys={setKeys} />
       </div>
     )
   })
