03afc646fefe274e3fdc815fd0a55493bb1907b7
Nevin Tay <nevintay@gmail.com>
2021-04-04 16:17:35 +0800

    Add event listener to dynamically set the value of the '--vh' css variable

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ function App() {
    * @description fetches instrument metadata
    */
   useEffect(() => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty("--vh", `${vh}px`)
+    window.addEventListener("resize", () => {
+      let vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty("--vh", `${vh}px`)
+    })
     try {
       fetchinstruments()
     } catch (err) {
