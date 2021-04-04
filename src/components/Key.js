31d6ac7ea2e605a51f09b445a96997deb0b51884
Nevin Tay <nevintay@gmail.com>
2021-04-04 18:11:13 +0800

    Add setKey() function as a prop and create function to handle history of pressed keys

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,7 +1,7 @@
 /* eslint react/prop-types: 0 */
 import React, { useEffect } from "react"
 
-const Key = ({ instrument = {} }) => {
+const Key = ({ instrument = {}, setKeys }) => {
   const playInstrument = (instrument) => {
     instrument.pause()
     instrument.currentTime = 0
@@ -24,11 +24,32 @@ const Key = ({ instrument = {} }) => {
     )
   }
 
+  const updateKeysHistory = (e) => {
+    if (e.key === instrument.keyboardPosition) {
+      setKeys((prev) => [instrument.keyboardPosition, ...prev])
+    }
+    return null
+  }
+
+  /**
+   * @description sets "keydown" event listener if instruments have been loaded from server
+   */
+  useEffect(() => {
+    if (instrument.size === 0) {
+      return
+    }
+    window.addEventListener("keydown", (e) => {
+      updateKeysHistory(e)
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [instrument])
+
   let el
   useEffect(() => {
     if (instrument.size === 0) {
       return
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     el = document.querySelector(`#${instrument.keyboardPosition}p`)
     document.addEventListener("keydown", (e) => {
       if (e.key === instrument.keyboardPosition) {
