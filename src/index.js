dc6da61f8efc7a56ffaa235c94ebedf71b7e9c53
Nevin Tay <nevintay@gmail.com>
2021-04-11 21:51:54 +0800

    Wrap InstrumentState around App

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
+import InstrumentsState from "./context/instruments/InstrumentsState"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <InstrumentsState>
+      <App />
+    </InstrumentsState>
   </React.StrictMode>,
   document.getElementById("root")
 )
