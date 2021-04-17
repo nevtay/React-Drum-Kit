2083a402abeb6627571a1cd3202ba4c75adf2ed8
Nevin Tay <nevintay@gmail.com>
2021-04-17 15:51:19 +0800

    Add InstrumentsContext to pull out relevant functions/variables

--- src/components/Keys.js
+++ src/components/Keys.js
@@ -1,7 +1,22 @@
-import React from "react"
+import React, { useEffect, useContext } from "react"
 import Key from "./Key"
+import InstrumentsContext from "../context/instruments/instrumentsContext"
 
-const Keys = ({ instruments, setKeys }) => {
+const Keys = ({ setKeys }) => {
+  const instrumentsContext = useContext(InstrumentsContext)
+  const { fetchInstruments, instruments } = instrumentsContext
+
+  /**
+   * @description fetches instrument metadata
+   */
+  useEffect(() => {
+    try {
+      fetchInstruments()
+    } catch (err) {
+      throw new Error(err)
+    }
+  }, [])
+
   return instruments.map((instrument) => {
     return (
       <div key={instrument.id}>
