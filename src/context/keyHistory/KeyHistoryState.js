f60e275af17896349c5cb940b72f870a3afb514f
Nevin Tay <nevintay@gmail.com>
2021-04-18 16:47:07 +0800

    Create function for dispatching CLEAR_KEYS

--- src/context/keyHistory/KeyHistoryState.js
+++ src/context/keyHistory/KeyHistoryState.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from "react"
 import KeyHistoryContext from "./keyHistoryContext"
 import keyHistoryReducer from "./keyHistoryReducer"
-import { SET_KEYS } from "./types"
+import { SET_KEYS, CLEAR_KEYS } from "./types"
 
 const KeyHistoryState = (props) => {
   const initialState = {
@@ -17,11 +17,18 @@ const KeyHistoryState = (props) => {
     })
   }
 
+  const clearKeyHistory = () => {
+    dispatch({
+      type: CLEAR_KEYS,
+    })
+  }
+
   return (
     <KeyHistoryContext.Provider
       value={{
         keyHistory: state.keyHistory,
         updateKeyHistory,
+        clearKeyHistory,
       }}
     >
       {props.children}
